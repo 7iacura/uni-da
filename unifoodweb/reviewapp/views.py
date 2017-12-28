d6d15d42cd36c9a9028a1515c293101a26dab248
7iacura <m.curatitoli@gmail.com>
2017-12-28 10:53:21 +0100

    Added django default configuration: with django installed, run 'python3 manage.py runserver' in correct path, and in http://127.0.0.1:8000/ address is possible to view default django page

---
+++ unifoodweb/reviewapp/views.py
@@ -0,0 +1,18 @@
+# from django.shortcuts import render
+#
+# # Create your views here.
+#
+# from django.http import HttpResponse
+#
+#
+# def index(request):
+# 	return HttpResponse("Hello, world. You're at the polls index.")
+
+from django.shortcuts import render
+from django.http import HttpResponse
+from django.template import loader
+
+
+def index(request):
+	template = loader.get_template('index.html')
+	return HttpResponse(template.render(request))
