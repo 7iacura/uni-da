cb6a8f743b6969fcabc18f5b77ac913b92714a2e
7iacura <m.curatitoli@gmail.com>
2017-12-28 14:15:21 +0100

    work in progress, tables in admin fixed

--- unifoodweb/reviewapp/admin.py
+++ unifoodweb/reviewapp/admin.py
@@ -4,6 +4,17 @@ from .models import User
 from .models import Product
 from .models import Rating
 
-admin.site.register(User)
-admin.site.register(Product)
-admin.site.register(Rating)
+
+class UserAdmin(admin.ModelAdmin):
+	list_display = ('id', 'num_rating', 'av_score', 'var_score')
+
+class ProductAdmin(admin.ModelAdmin):
+	list_display = ('id', 'num_rating', 'av_score', 'var_score')
+
+class RatingAdmin(admin.ModelAdmin):
+	list_display = ('id', 'productid', 'userid', 'score', 'text')
+
+
+admin.site.register(User, UserAdmin)
+admin.site.register(Product, ProductAdmin)
+admin.site.register(Rating, RatingAdmin)
