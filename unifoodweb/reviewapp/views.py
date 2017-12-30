4e67504a9d9e11a43d50ad4e88929a07e999731b
Mattia Curatitoli <m.curatitoli@outlook.it>
2017-12-30 09:35:02 +0100

    1.0
    - stile figo, con menu in alto
    - tre tabelle di view fatte

--- unifoodweb/reviewapp/views.py
+++ unifoodweb/reviewapp/views.py
@@ -1,13 +1,29 @@
-# from django.shortcuts import render
+from django.shortcuts import render
 from django.http import HttpResponse
-# from django.template import loader
+from django.template import loader
+from django.core.paginator import Paginator
+
+# from django.shortcuts import get_list_or_404, get_object_or_404
+
+from .models import User, Product, Rating
+
 
 def index(request):
-	return HttpResponse("Hello, world. Here we put a graphic fiqua.")
+	template = loader.get_template('reviewapp/index.html')
+	context = { }
+	return HttpResponse(template.render(context, request))
 
 
 def users(request):
-	return HttpResponse("List of users here")
+	object_list = User.objects.all()
+	template = loader.get_template('reviewapp/users.html')
+	paginator = Paginator(object_list, 25)
+	page = request.GET.get('page')
+	object_in_page = paginator.get_page(page)
+	context = {
+		'objects': object_in_page,
+	}
+	return HttpResponse(template.render(context, request))
 
 
 def user(request, user_id):
@@ -15,7 +31,15 @@ def user(request, user_id):
 
 
 def products(request):
-	return HttpResponse("List of products here")
+	product_list = Product.objects.all()
+	template = loader.get_template('reviewapp/products.html')
+	paginator = Paginator(product_list, 25)
+	page = request.GET.get('page')
+	product_page = paginator.get_page(page)
+	context = {
+		'objects': product_page,
+	}
+	return HttpResponse(template.render(context, request))
 
 
 def product(request, product_id):
@@ -23,7 +47,15 @@ def product(request, product_id):
 
 
 def ratings(request):
-	return HttpResponse("List of ratings here")
+	review_list = Rating.objects.all()
+	template = loader.get_template('reviewapp/ratings.html')
+	paginator = Paginator(review_list, 25)
+	page = request.GET.get('page')
+	review_page = paginator.get_page(page)
+	context = {
+		'objects': review_page,
+	}
+	return HttpResponse(template.render(context, request))
 
 
 def rating(request, rating_id):
