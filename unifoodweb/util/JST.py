e68e1f2d65b66253f815057e2448b15cf79a3bc7
danydepo@gmail.com <katenkyokotsu77?>
2018-02-18 16:00:52 +0100

    FIX JST/Sentiment

--- unifoodweb/util/JST.py
+++ unifoodweb/util/JST.py
@@ -1,8 +1,3 @@
-"""
-Implementation of the collapsed Gibbs sampler for Sentiment-LDA, described in
-Sentiment Analysis with Global Topics and Local Dependency (Li, Huang and Zhu)
-"""
-
 import numpy as np
 from nltk.corpus import stopwords
 import re
@@ -18,33 +13,16 @@ MAX_VOCAB_SIZE = 50000
 
 
 def sampleFromDirichlet(alpha):
-    """
-    Sample from a Dirichlet distribution
-    alpha: Dirichlet distribution parameter (of length d)
-    Returns:
-    x: Vector (of length d) sampled from dirichlet distribution
-
-    """
     return np.random.dirichlet(alpha)
 
 
 def sampleFromCategorical(theta):
-    """
-    Samples from a categorical/multinoulli distribution
-    theta: parameter (of length d)
-    Returns:
-    x: index ind (0 <= ind < d) based on probabilities in theta
-    """
     theta = theta/np.sum(theta)
     return np.random.multinomial(1, theta).argmax()
 
 
 def word_indices(wordOccuranceVec):
-    """
-    Turn a document vector of size vocab_size to a sequence
-    of word indices. The word indices are between 0 and
-    vocab_size-1. The sequence length is equal to the document length.
-    """
+
     wordOccuranceVec = wordOccuranceVec.toarray()[0]
     for idx in wordOccuranceVec.nonzero()[0]:
         for i in range(int(wordOccuranceVec[idx])):
@@ -54,16 +32,7 @@ def word_indices(wordOccuranceVec):
 class Jst:
 
     def __init__(self, numTopics, alpha, beta, gamma, numSentiments=2):
-        """
-        numTopics: Number of topics in the model
-        numSentiments: Number of sentiments (default 2)
-        alpha: Hyperparameter for Dirichlet prior on topic distribution
-        per document
-        beta: Hyperparameter for Dirichlet prior on vocabulary distribution
-        per (topic, sentiment) pair
-        gamma:Hyperparameter for Dirichlet prior on sentiment distribution
-        per (document, topic) pair
-        """
+
         self.alpha = alpha
         self.beta = beta
         self.gamma = gamma
@@ -71,55 +40,14 @@ class Jst:
         self.numSentiments = numSentiments
 
     def processSingleReview(self, review, d=None):
-        """
-        Convert a raw review to a string of words
-        """
         letters_only = re.sub("[^a-zA-Z]", " ", review)
         words = letters_only.lower().split()
         stops = set(stopwords.words("english"))
         meaningful_words = [st.stem(w) for w in words if w not in stops]
         return(" ".join(meaningful_words))
-        # review = review.decode("ascii", "ignore").encode("ascii")
-            
-        # stops = set(stopwords.words("english"))
-        # meaningful_words = []
-        # sentences = sent_tokenize(review)
-        # i = 0
-        # for sentence in sentences:
-        #     pos_words = pos_tag(word_tokenize(sentence))
-        #     for (word, pos) in pos_words:
-        #         word = word.lower()
-        #         word = re.sub("[^a-zA-Z]", "", word)
-        #         if word == "":
-        #             continue
-        #         if word.lower() in stops:
-        #             continue
-        #         wordnetpos = None
-        #         if pos.startswith('N'):
-        #             wordnetpos = 'n'
-        #         elif pos.startswith('V'):
-        #             wordnetpos = 'v'
-        #         elif pos.startswith('J'):
-        #             wordnetpos = 'a'
-        #         elif pos.startswith('R'):
-        #             wordnetpos = 'r'
-        #         synsets = swn.senti_synsets(word, wordnetpos)
-        #         posScore = np.mean([s.pos_score() for s in synsets])
-        #         negScore = np.mean([s.neg_score() for s in synsets])
-        #         if d is not None and posScore >= 0.1 and posScore > negScore:
-        #             self.priorSentiment[(d, i)] = 1
-        #         elif d is not None and negScore >= 0.1 and negScore > posScore:
-        #             self.priorSentiment[(d, i)] = 0
-        #         meaningful_words.append(st.stem(word))
-        #         i += 1
-        # return(" ".join(meaningful_words))
 
-    def processReviews(self, reviews, saveAs=None, saveOverride=False):
-        import os
-        import dill
-        if not saveOverride and saveAs and os.path.isfile(saveAs):
-            [wordOccurenceMatrix, self.vectorizer] = dill.load(open(saveAs,'r'))
-            return wordOccurenceMatrix
+
+    def processReviews(self, reviews):
         processed_reviews = []
         i = 0
         for review in reviews:
@@ -134,8 +62,6 @@ class Jst:
                                           max_features=MAX_VOCAB_SIZE)
         train_data_features = self.vectorizer.fit_transform(processed_reviews)
         wordOccurenceMatrix = train_data_features
-        if saveAs:
-            dill.dump([wordOccurenceMatrix, self.vectorizer], open(saveAs, 'w'))
         return wordOccurenceMatrix
 
     def use_vader(self, sentence):
@@ -154,12 +80,8 @@ class Jst:
 
 
 
-    def _initialize_(self, reviews, saveAs=None, saveOverride=False):
-        """
-        wordOccuranceMatrix: numDocs x vocabSize matrix encoding the
-        bag of words representation of each document
-        """
-        self.wordOccuranceMatrix = self.processReviews(reviews, saveAs, saveOverride)
+    def _initialize_(self, reviews):
+        self.wordOccuranceMatrix = self.processReviews(reviews)
         numDocs, vocabSize = self.wordOccuranceMatrix.shape
 
 
@@ -205,10 +127,7 @@ class Jst:
 
 
     def conditionalDistribution(self, d, v):
-        """
-        Calculates the (topic, sentiment) probability for word v in document d
-        Returns:    a matrix (numTopics x numSentiments) storing the probabilities
-        """
+
         probabilities_ts = np.ones((self.numTopics, self.numSentiments))
         firstFactor = (self.n_dt[d] + self.alpha) / \
             (self.n_d[d] + self.numTopics * self.alpha)
@@ -227,28 +146,8 @@ class Jst:
 
 
 
-    def getTopKWordsByLikelihood(self, K):
-        """
-        Returns top K discriminative words for topic t and sentiment s
-        ie words v for which p(t, s | v) is maximum
-        """
-        pseudocounts = np.copy(self.n_vts)
-        normalizer = np.sum(pseudocounts, (1, 2))
-        pseudocounts /= normalizer[:, np.newaxis, np.newaxis]
-        response= []
-        for t in range(self.numTopics):
-            for s in range(self.numSentiments):
-                topWordIndices = pseudocounts[:, t, s].argsort()[-1:-(K + 1):-1]
-                vocab = self.vectorizer.get_feature_names()
-                response.append((t, s, [vocab[i] for i in topWordIndices]))
-        return response
-
-
     def getTopKWords(self, K):
-        """
-        Returns top K discriminative words for topic t and sentiment s
-        ie words v for which p(v | t, s) is maximum
-        """
+
         pseudocounts = np.copy(self.n_vts)
         normalizer = np.sum(pseudocounts, (0))
         pseudocounts /= normalizer[np.newaxis, :, :]
@@ -261,11 +160,9 @@ class Jst:
         return response
 
 
-    def run(self, reviews, score, maxIters=30, saveAs=None, saveOverride=False):
-        """
-        Runs Gibbs sampler for sentiment-LDA
-        """
-        self._initialize_(reviews, saveAs, saveOverride)
+    def run(self, reviews, score, maxIters=30):
+
+        self._initialize_(reviews)
         numDocs, vocabSize = self.wordOccuranceMatrix.shape
         pprint(vocabSize)
         for iteration in range(maxIters):
